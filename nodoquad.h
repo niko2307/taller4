ef042e27b8d57c365b121a85e6463bc34edab5d6
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 14:36:06 -0500

    Update nodoquad.h

--- nodoquad.h
+++ nodoquad.h
@@ -1,1 +1,28 @@
-
+#ifndef NODOQUAD_H
+#define NODOQUAD_H
+classNodoQuad{
+  protected: 
+punto dato; 
+NodoQuad* hijoSupIzq;
+NodoQuad* hijoSupDer; 
+NodoQuad* hijoInfIzq; 
+NodoQuad* hijoInfDer;
+  public: 
+NodoQuad();
+NodoQuad(punto val); 
+~NodoQuad();
+punto obtenerDato(); 
+voidfijarDato(punto val); 
+NodoQuad* obtenerHijoSupIzq(); 
+NodoQuad* obtenerHijoSupDer(); 
+NodoQuad* obtenerHijoInfIzq(); 
+NodoQuad* obtenerHijoInfDer(); 
+void fijarHijoSupIzq(NodoQuad* sizq); 
+void  fijarHijoSupDer(NodoQuad* sder); 
+void fijarHijoInfIzq(NodoQuad* iizq);
+void fijarHijoInfDer(NodoQuad* ider);
+bool esHoja();
+  
+  
+};
+#endif // NODOQUAD.H
