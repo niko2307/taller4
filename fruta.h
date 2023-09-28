f3e553b0c09e991a19bd7c66f13d34f5cfce3261
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 22:35:45 -0500

    Update fruta.h

--- fruta.h
+++ fruta.h
@@ -1,1 +1,23 @@
+#ifndef FRUTA_H
+#define FRUTA_H
 
+#include <string>
+
+class Fruta {
+private:
+    std::string clase; 
+    double peso;       
+    int color;         
+
+public:
+    Fruta();
+    Fruta( std::string& tipo, double p, int col);
+    std::string obtenerClase() ;
+    double obtenerPeso() ;
+    int obtenerColor() ;
+    void fijarClase( std::string& tipo);
+    void fijarPeso(double p);
+    void fijarColor(int col);
+};
+
+#endif // FRUTA_H
