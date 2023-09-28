14cf2120b8afe54f51b1c72d87996bfbfed85946
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 20:06:15 -0500

    Update nodokd.h

--- nodokd.h
+++ nodokd.h
@@ -1,1 +1,27 @@
+#ifndef NODOKD_H
+#define NODOKD_H
+#include "punto.h"
 
+template <typename T>
+class NodoKD {
+    
+protected:
+Punto dato;
+NodoKD* hijoIzq;
+NodoKD* hijoDer;
+
+public:
+NodoKD();
+NodoKD(Punto val);
+~NodoKD();
+bool esHoja();
+Punto obtenerDato();
+void fijarDato(Punto val);
+NodoKD* obtenerHijoIzq();
+NodoKD* obtenerHijoDer();
+void fijarHijoIzq(NodoKD* izq);
+void fijarHijoDer(NodoKD* der);
+
+};
+
+#endif // NODOKD_H
