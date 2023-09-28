8070eff41cc93dfe3a168f4d8c69c00406203c52
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 20:06:00 -0500

    Update arbolquad.h

--- arbolquad.h
+++ arbolquad.h
@@ -1,1 +1,27 @@
+#ifndef ARBOLQUAD_H
+#define ARBOLQUAD_H
+#include "punto.h"
+#include "nodoquad.h"
 
+class ArbolQuad {
+protected:
+NodoQuad* raiz;
+
+public:
+ArbolQuad();
+ArbolQuad(Punto val);
+~ArbolQuad();
+Punto datoRaiz();
+NodoQuad* obtenerRaiz();
+void fijarRaiz(NodoQuad* n_raiz);
+bool esVacio();
+bool insertar(Punto val);
+void preOrden();
+void inOrden();
+void posOrden();
+void nivelOrden();
+};
+
+#include "arbolquad.hxx"
+
+#endif // ARBOLQUAD.H
