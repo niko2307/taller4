5aa6f3ee542beb210dd3fd8b6035bb73542a1ee6
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 22:17:58 -0500

    Update arbolquad.hxx

--- arbolquad.hxx
+++ arbolquad.hxx
@@ -1,1 +1,40 @@
+#include "arbolkd.hxx"
+
+template <class T>
+bool ArbolQuad<T>::esVacio() {
+    return raiz == NULL;
+}
+
+template <class T>
+NodoQuad<T>* ArbolQuad<T>::obtenerRaiz() {
+    return raiz;
+}
+
+template <class T>
+void ArbolQuad<T>::fijarRaiz(NodoQuad<T>* n_raiz) {
+    raiz = n_raiz;
+}
+
+
+template <class T>
+Punto ArbolQuad<T>::datoRaiz() {
+    if (raiz) {
+        int x = *(raiz->obtenerx());
+        int y = *(raiz->obtenery());
+
+        return Punto(x, y);
+    } else {
+        
+        return Punto(0, 0);
+    }
+}
+
+template <class T>
+ArbolQuad<T>::~ArbolQuad() {  
+}
+
+template <class T>
+ArbolQuad<T>::ArbolQuad() {
+    this -> raiz=NULL;
+}
 
