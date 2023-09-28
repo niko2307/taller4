a95874901c41f8c58a3631be47022ee4c25a685a
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 20:05:22 -0500

    Update arbolkd.h

--- arbolkd.h
+++ arbolkd.h
@@ -1,1 +1,30 @@
+#ifndef ARBOLKD_H
+#define ARBOLKD_H
+#include "punto.h"
+#include "nodokd.h"
+
+
+class ArbolKD {
+protected:
+NodoKD* raiz;
+
+
+public:
+ArbolKD();
+ArbolKD(Punto val);
+~ArbolKD();
+Punto datoRaiz();
+NodoKD* obtenerRaiz();
+void fijarRaiz(NodoKD* n_raiz);
+bool esVacio();
+bool insertar(Punto val);
+void preOrden();
+void inOrden();
+void posOrden();
+void nivelOrden();
+
+};
+#include"arbolkd.hxx"
+
+#endif//ARBOLKD_H
 
