57697b2b0785569d38d979af1518fd24f58d1455
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 22:35:32 -0500

    Update fruta.cxx

--- fruta.cxx
+++ fruta.cxx
@@ -1,1 +1,36 @@
+#include "fruta.h"
 
+
+Fruta::Fruta( std::string& tipo, double p, int col) {
+    clase = tipo;
+    peso = p;
+    color = col;
+}
+Fruta::Fruta(){
+    
+}
+
+
+std::string Fruta::obtenerClase()  {
+    return clase;
+}
+
+double Fruta::obtenerPeso()  {
+    return peso;
+}
+
+int Fruta::obtenerColor()  {
+    return color;
+}
+
+void Fruta::fijarClase( std::string& tipo) {
+    clase = tipo;
+}
+
+void Fruta::fijarPeso(double p) {
+    peso = p;
+}
+
+void Fruta::fijarColor(int col) {
+    color = col;
+}
