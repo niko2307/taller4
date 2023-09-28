dd73f9aeb1c264682429cf3701f827fd3a6a7d05
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 22:18:25 -0500

    Update nodokd.hxx

--- nodokd.hxx
+++ nodokd.hxx
@@ -1,1 +1,55 @@
+#include "nodokd.h"
+template <class T>
 
+NodoKD<T>::NodoKD(Punto val){
+dato= val;
+hijoIzq = NULL;
+hijoDer=NULL;
+
+}
+template <class T>
+NodoKD<T>::NodoKD(){
+hijoIzq = NULL;
+hijoDer=NULL;
+}
+
+template <class T>
+ bool NodoKD<T>::esHoja(){
+     if (hijoDer == NULL && hijoIzq == NULL)
+        return true;
+    else
+       return false;   
+ }
+
+template <class T>
+Punto NodoKD<T>::obtenerDato(){
+    return dato;
+}
+
+template <class T>
+void NodoKD<T>::fijarDato(Punto val){
+    dato=val;
+
+}
+
+template <class T>
+NodoKD<T> *NodoKD<T>::obtenerHijoDer(){
+    return hijoDer;
+}
+
+template <class T>
+NodoKD<T> *NodoKD<T>::obtenerHijoIzq(){
+    return hijoIzq;
+}
+
+template <class T>
+void NodoKD<T>:: fijarHijoDer(NodoKD* der){
+      hijoDer =der;
+
+}
+
+template <class T>
+void NodoKD<T>:: fijarHijoDer(NodoKD* izq){
+      hijoIzq = izq;
+
+}
