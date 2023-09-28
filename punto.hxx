269b39132a243da00ef7a303ef3944251ae06e21
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-27 22:19:49 -0500

    Update punto.hxx

--- punto.hxx
+++ punto.hxx
@@ -1,6 +1,9 @@
 #include "punto.h"
 
-Punto::Punto(int x, int y) : x(x), y(y) {}
+Punto::Punto(int x, int y)  {
+   this->x = x;
+    this->y = y;
+}
 
 Punto& Punto::operator=(const Punto& p) {
     if (this == &p) {
@@ -18,4 +21,20 @@ bool Punto::operator==(const Punto& p) const {
 std::ostream& operator<<(std::ostream& o, const Punto& p) {
     o << "(" << p.x << "," << p.y << ")";
     return o;
+}
+
+int* Punto::obtenerx(){
+    return &x ;
+}
+
+void Punto::fijarx(Punto sx) {
+    x = *sx.obtenerx();
+}
+
+int* Punto::obtenery() {
+    return &y;
+}
+
+void Punto::fijary(Punto sy) {
+    y = *sy.obtenery();
 }
