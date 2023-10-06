019f5e47051985c26e95e5ceef6eebc98afb7663
JOHNY <jonathanjurados@gmail.com>
2023-10-06 18:34:05 -0500

    save

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[]) {
 
        
           bool insertQUAD=arbolQuad.insertar(val);
-          bool insertkd= arbolkd.insert(val);
+          bool insertkd= arbolkd.insert( peso,  color,clase );
           
 
 
