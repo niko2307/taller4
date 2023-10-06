c9d2b2d73c5ff2d59fc8ec3cdcfb0968cdb8dc88
Anton Patrignani <82620970+CallMeSag3@users.noreply.github.com>
2023-10-06 11:02:28 -0500

    search arbolquad

--- arbolquad.h
+++ arbolquad.h
@@ -1,27 +1,40 @@
+
 #ifndef ARBOLQUAD_H
 #define ARBOLQUAD_H
 #include "punto.h"
 #include "nodoquad.h"
+#include "fruta.h"
+typedef enum
+{
+    CUADRANTE_1,
+    CUADRANTE_2,
+    CUADRANTE_3,
+    CUADRANTE_4
+} Cuadrante;
 
-class ArbolQuad {
-protected:
-NodoQuad* raiz;
+template <class T>
+class ArbolQuad
+{
+private:
+    NodoQuad<T> *raiz;
 
 public:
-ArbolQuad();
-ArbolQuad(Punto val);
-~ArbolQuad();
-Punto datoRaiz();
-NodoQuad* obtenerRaiz();
-void fijarRaiz(NodoQuad* n_raiz);
-bool esVacio();
-bool insertar(Punto val);
-void preOrden();
-void inOrden();
-void posOrden();
-void nivelOrden();
+    ArbolQuad();
+    bool insertar(T &val);
+    Cuadrante calcularCuadrante(Punto coordenadas);
+    NodoQuad<T> *insertarRecursivo(NodoQuad<T> *actual, T val, Cuadrante cuadrante);
+    ~ArbolQuad();
+    Punto datoRaiz();
+    NodoQuad<T> *obtenerRaiz();
+    void fijarRaiz(NodoQuad<T> *n_raiz);
+    NodoQuad<T> *buscar(Punto p);
+    bool esVacio();
+    void preOrden();
+    void inOrden();
+    void posOrden();
+    void nivelOrden();
+    std::string buscarQuad(T &val);
+    void buscarRecursivo(NodoQuad<T> *actual, double peso, std::string color, std::string &nombreFrutaCercana, double &pesoFrutaCercana, double &distanciaMinima);
 };
-
 #include "arbolquad.hxx"
-
 #endif // ARBOLQUAD.H
